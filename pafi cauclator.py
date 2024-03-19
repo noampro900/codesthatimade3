ee752a7c7dae2b2752e9c4070b21772ef5e886b3
noampro900 <76394229+noampro900@users.noreply.github.com>
2024-03-19 16:35:57 +0200

    Add files via upload

---
+++ pafi cauclator.py
@@ -0,0 +1,98 @@
+
+import time
+def yyyyyyyyyyy():
+    time.sleep(2)
+
+def yuyu():
+    time.sleep(3)
+
+
+
+
+print("pafi: welcome to my cauclator")
+time.sleep(3)
+print("pafi: just  remembear somthing do not type 3333 in the + or - ok?")
+time.sleep(2)
+print("pafi: ?")
+time.sleep(3)
+print("pafi: i take that as ok")
+time.sleep(2)
+
+
+def m():
+    f = int(input("what number you want first:    "))
+    t = input("do you want to + or - ? :      ")
+    g = int(input("sec num if you know what i mean:      "))
+    if t=="+":
+        print(f+g)
+
+    elif t=="-":
+        print(f-g)
+
+    elif t=="3333":
+        print("pafi: NO!!")
+        time.sleep(2)
+        print("pafi: DONT YOU DARE!!")
+
+    elif t=="what happend when you write_3333?":
+        print("pali: wanna see?")
+        time.sleep(1.99)
+        oo = input("pali: yes or no?     ")
+        if oo=="yes":
+            print("pali: ok lets go")
+            time.sleep(2.5)
+            def ikj():
+                rgrti = int(input("first num:   "))
+                hjrieg9h = input("you want to + or - or * or / or **:    ")
+                efrhbgurti = int(input("sec num:    "))
+                if hjrieg9h=="*":
+                    print(rgrti*efrhbgurti)
+
+
+
+                elif hjrieg9h=="/":
+                    print(rgrti/efrhbgurti)
+
+
+
+
+                elif hjrieg9h=="**":
+                    print(rgrti**efrhbgurti)
+
+                elif hjrieg9h=="+":
+                    print(rgrti+efrhbgurti)
+
+
+                elif hjrieg9h=="-":
+                    print(rgrti-efrhbgurti)
+
+
+                elif hjrieg9h=="paficomeback":
+                    print("pafi: here i come where are you?")
+                    yyyyyyyyyyy()
+                    print("what are you serious??")
+                    yyyyyyyyyyy()
+                    print("ok but never come to this place again!")
+                    
+                    
+
+                else:
+                    print("pali: hm")
+
+            print(ikj())
+        elif oo=="no":
+            print("pafi: where are you!!")
+            time.sleep(2)
+            print("pali: hmmmm")
+            time.sleep("2")
+            print("grrrrr!")
+        else:
+            print("?")
+
+    else:
+        print("hmmm error?")
+
+
+
+print(m())
+
